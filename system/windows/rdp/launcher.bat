971e5d61579e28decfd0a91c2a0ff26660c08616
Tony LELIEVRE <131965927+tonyeffinity@users.noreply.github.com>
2026-02-09 11:59:06 +0100

    Update GITHUB_URL to point to the correct path

--- system/windows/rdp/launcher.bat
+++ system/windows/rdp/launcher.bat
@@ -5,7 +5,7 @@ REM --- CONFIGURATION ---
 SET PROJECT_ID=nom-projet
 SET VM_NAME=nom-vm
 SET ZONE=europe-west1-c
-SET GITHUB_URL=https://raw.githubusercontent.com/effinity-fr/dev-toolkit/refs/heads/main/rdp-windows/connect.ps1
+SET GITHUB_URL=https://raw.githubusercontent.com/effinity-fr/dev-toolkit/refs/heads/main/system/windows/rdp/connect.ps1
 
 echo [Mise a jour du script de securite...]
 powershell -NoProfile -ExecutionPolicy Bypass -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; $s = Invoke-WebRequest -Uri '%GITHUB_URL%' -UseBasicParsing; Invoke-Expression ($s.Content + ' -projectName %PROJECT_ID% -instanceName %VM_NAME% -zone %ZONE%')"
